789fa784b4005a12ed4e1f8d0fdec795e33e1259
Karol Górka <karol.gorka@microsoft.wsei.edu.pl>
2024-01-10 15:53:34 +0100

    Add project files.

---
+++ mvc 10.01.2024/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace mvc_10._01._2024.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ mvc 10.01.2024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvc_10._01._2024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
